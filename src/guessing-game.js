93e97d13367d6f5ecf70f6211bfc67ef5dc5c57f
Eugene Farafonov <eu.farafonov@gmail.com>
2019-10-02 11:44:25 +0300

    init: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.middle = null;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.max + this.min) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
